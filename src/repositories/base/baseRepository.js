cd867e2a17b1fba720857fab8c6ca8db80bf6272
Junior Paula <luke.junnior@icloud.com>
2022-12-26 14:56:24 -0300

    Implements BaseRepository protocol class

---
+++ src/repositories/base/baseRepository.js
@@ -0,0 +1,14 @@
+import { readFile } from 'fs/promises'
+
+export class BaseRepositoryProtocol {
+  constructor({ file }) {
+    this.file = file
+  }
+
+  async find(itemId) {
+    const content = JSON.parse(await readFile(this.file))
+    if (!itemId) return content
+
+    return content.find(({ id }) => id === itemId)
+  }
+}
