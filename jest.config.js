c7e3e66a0e256bf1bfa121beb812d56c5489d764
Junior Paula <luke.junnior@icloud.com>
2022-12-26 11:24:09 -0300

    Add and config jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  collectCoverageFrom: ['src/**/*.js', '!src/index.js'],
+  coverageThreshold: {
+    global: {
+      branch: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  maxWorkers: '50%',
+  testEnvironment: 'jest-environment-node',
+  watchPathIgnorePatterns: ['node_modules'],
+  transformIgnorePatterns: ['node_modules'],
+};
