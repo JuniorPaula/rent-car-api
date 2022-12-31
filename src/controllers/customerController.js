3ddc431afc231d6161de1fa83740c0765939b171
Junior Paula <luke.junnior@icloud.com>
2022-12-30 21:03:17 -0300

    Simplify code

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -2,12 +2,11 @@ import { HttpResponseStatus } from '../utils/httpResponseStatus.js'
 
 export class CustomerController {
   async handle(httpRequest) {
-    const { name, age } = httpRequest.body
-    if (!name) {
-      return HttpResponseStatus.badRequest('name')
-    }
-    if (!age) {
-      return HttpResponseStatus.badRequest('age')
+    const fields = ['name', 'age']
+    for (const field of fields) {
+      if (!httpRequest.body[field]) {
+        return HttpResponseStatus.badRequest(field)
+      }
     }
   }
 }
