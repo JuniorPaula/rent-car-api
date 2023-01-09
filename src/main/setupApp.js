7713a906ec6dbf3c5b4728c085eaf7e964f6e0e2
Junior Paula <luke.junnior@icloud.com>
2023-01-09 14:46:05 -0300

    Ensure setupContentType enable json content type as default

--- src/main/setupApp.js
+++ src/main/setupApp.js
@@ -1,5 +1,7 @@
 import setupJsonParse from '../middlewares/jsonParse.js'
+import setupContentType from '../middlewares/contentType.js'
 
 export const setupApp = (app) => {
+  app.use(setupContentType)
   app.use(setupJsonParse)
 }
---
+++ src/middlewares/contentType.test.js
@@ -0,0 +1,18 @@
+import { describe, test, beforeEach, jest } from '@jest/globals'
+import request from 'supertest'
+
+describe('ContentType Middleware', () => {
+  let app
+
+  beforeEach(async () => {
+    jest.resetModules()
+    app = (await import('../app.js')).default
+  })
+  test('Should enabled json content type as default', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app).get('/test_content_type').expect('content-type', /json/)
+  })
+})
