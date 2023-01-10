c19a88adc6cc11e2e4e600c5b9b4df06e9eda222
Junior Paula <luke.junnior@icloud.com>
2023-01-10 08:43:44 -0300

    Move car category to a onwer folder

--- src/factories/carCategoryFactory.js
+++ src/factories/carCategoryFactory.js
@@ -1,6 +1,6 @@
 import { CarCategoryRepository } from '../repositories/carCategoryRepository.js'
 import { CarCategoryEntity } from '../entities/carCategory.js'
-import { CarCategoryController } from '../controllers/carCategoryController.js'
+import { CarCategoryController } from '../controllers/car-category/carCategoryController.js'
 
 export const carCategoryFactory = () => {
   const carCategoryRepository = new CarCategoryRepository()
