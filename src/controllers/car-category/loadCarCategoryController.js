a84791b390e7bc48b12e25c80fd0b3d3d11e9386
Junior Paula <luke.junnior@icloud.com>
2023-01-10 09:08:23 -0300

    Ensure LoadCarCategoryController calls CarCategory entity with correct param

--- src/controllers/car-category/loadCarCategoryController.js
+++ src/controllers/car-category/loadCarCategoryController.js
@@ -1,9 +1,16 @@
 import { HttpResponseStatus } from '../../utils/httpResponseStatus.js'
 
 export class LoadCarCategoryController {
+  constructor(carCategoryEntity) {
+    this.carCategoryEntity = carCategoryEntity
+  }
+
   async handle(httpRequest) {
     if (!httpRequest) {
       return HttpResponseStatus.serverError()
     }
+
+    const { carCategoryId } = httpRequest.params
+    await this.carCategoryEntity.getCarCategory({ carCategoryId })
   }
 }
--- src/controllers/car-category/loadCarCategoryController.spec.js
+++ src/controllers/car-category/loadCarCategoryController.spec.js
@@ -1,11 +1,22 @@
-import { describe, test, expect, beforeEach } from '@jest/globals'
+import { describe, test, expect, beforeEach, jest } from '@jest/globals'
 import { LoadCarCategoryController } from './loadCarCategoryController.js'
 
+const mockCarCategoryEntityStub = () => {
+  class CarCategoryEntityStub {
+    async getCarCategory({ carCategoryId = null }) {
+      this.carCategoryId = carCategoryId
+    }
+  }
+
+  return new CarCategoryEntityStub()
+}
+
 describe('LoadCarCategoryController', () => {
+  let carCategoryEntityStub = mockCarCategoryEntityStub()
   let sut = {}
 
   beforeEach(() => {
-    sut = new LoadCarCategoryController()
+    sut = new LoadCarCategoryController(carCategoryEntityStub)
   })
 
   test('Should return 500 if no httpRequest is provided', async () => {
@@ -13,5 +24,21 @@ describe('LoadCarCategoryController', () => {
 
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.error.message).toBe('Internal server error')
+  })
+
+  test('Should call CarCategoryEntity with correct param', async () => {
+    const httpRequest = {
+      params: {
+        carCategoryId: '123-asdf-098',
+      },
+    }
+    const getSpy = jest.spyOn(
+      carCategoryEntityStub,
+      carCategoryEntityStub.getCarCategory.name,
+    )
+    await sut.handle(httpRequest)
+
+    expect(getSpy).toHaveBeenCalledWith({ carCategoryId: '123-asdf-098' })
+    expect(getSpy).toHaveBeenCalledTimes(1)
   })
 })
