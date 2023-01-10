3e11bc34b61d644ace62c01f3dbcc247dbfdcdcf
Junior Paula <luke.junnior@icloud.com>
2023-01-10 15:03:58 -0300

    Add params on adpter routes request

--- src/adapters/express-routes-adapters.js
+++ src/adapters/express-routes-adapters.js
@@ -2,6 +2,7 @@ export const adapterRoutes = (controller) => {
   return async (req, res) => {
     const httpRequest = {
       body: req.body,
+      params: req.params,
     }
     const httpResponse = await controller.handle(httpRequest)
     res.status(httpResponse.statusCode).json(httpResponse.body)
