54c1a7abc5896cf98447299410c835e2e830b943
Junior Paula <luke.junnior@icloud.com>
2023-01-10 11:44:38 -0300

    Move tests to a onwers describes

--- src/entities/carCategory.spec.js
+++ src/entities/carCategory.spec.js
@@ -21,52 +21,60 @@ describe('CarCategory Entity', () => {
     sut = new CarCategoryEntity(carCategoryRepositoryStub)
   })
 
-  test('Should call CarCategoryRepository with correct values', async () => {
-    const spyCarCategory = jest.spyOn(
-      carCategoryRepositoryStub,
-      carCategoryRepositoryStub.save.name,
-    )
+  describe('#Create car category', () => {
+    test('Should call CarCategoryRepository with correct values', async () => {
+      const spyCarCategory = jest.spyOn(
+        carCategoryRepositoryStub,
+        carCategoryRepositoryStub.save.name,
+      )
 
-    await sut.create({ categoryName: 'Crew Cab Pickup', price: '150.90' })
+      await sut.create({ categoryName: 'Crew Cab Pickup', price: '150.90' })
 
-    expect(spyCarCategory).toHaveBeenCalledWith({
-      categoryName: 'Crew Cab Pickup',
-      price: '150.90',
-    })
-  })
-
-  test('Should throw if no param are provided', async () => {
-    const spy = sut.create({})
-    await expect(spy).rejects.toThrow('missing param: categoryName/price')
-  })
-
-  test('Should throw if no repository are provided on constructor', async () => {
-    const sut = new CarCategoryEntity()
-    const spy = sut.create({ categoryName: 'Crew Cab Pickup', price: '150.90' })
-
-    await expect(spy).rejects.toThrow('missing param: carCategoryRepository')
-  })
-
-  test('Should throw if CarCategoryRepository has no method save', async () => {
-    class CarCategoryRepositoryStub {}
-    const fakeRepository = new CarCategoryRepositoryStub()
-
-    const sut = new CarCategoryEntity(fakeRepository)
-    const spy = sut.create({ categoryName: 'Crew Cab Pickup', price: '150.90' })
-
-    await expect(spy).rejects.toThrow('missing param: carCategoryRepository')
-  })
-
-  test('Should throws if CarCategoryRepository to throws', async () => {
-    jest
-      .spyOn(carCategoryRepositoryStub, carCategoryRepositoryStub.save.name)
-      .mockImplementationOnce(() => Promise.reject(new Error()))
-
-    const promise = sut.create({
-      categoryName: 'Crew Cab Pickup',
-      price: '150.90',
+      expect(spyCarCategory).toHaveBeenCalledWith({
+        categoryName: 'Crew Cab Pickup',
+        price: '150.90',
+      })
     })
 
-    await expect(promise).rejects.toThrow()
+    test('Should throw if no param are provided', async () => {
+      const spy = sut.create({})
+      await expect(spy).rejects.toThrow('missing param: categoryName/price')
+    })
+
+    test('Should throw if no repository are provided on constructor', async () => {
+      const sut = new CarCategoryEntity()
+      const spy = sut.create({
+        categoryName: 'Crew Cab Pickup',
+        price: '150.90',
+      })
+
+      await expect(spy).rejects.toThrow('missing param: carCategoryRepository')
+    })
+
+    test('Should throw if CarCategoryRepository has no method save', async () => {
+      class CarCategoryRepositoryStub {}
+      const fakeRepository = new CarCategoryRepositoryStub()
+
+      const sut = new CarCategoryEntity(fakeRepository)
+      const spy = sut.create({
+        categoryName: 'Crew Cab Pickup',
+        price: '150.90',
+      })
+
+      await expect(spy).rejects.toThrow('missing param: carCategoryRepository')
+    })
+
+    test('Should throws if CarCategoryRepository to throws', async () => {
+      jest
+        .spyOn(carCategoryRepositoryStub, carCategoryRepositoryStub.save.name)
+        .mockImplementationOnce(() => Promise.reject(new Error()))
+
+      const promise = sut.create({
+        categoryName: 'Crew Cab Pickup',
+        price: '150.90',
+      })
+
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
