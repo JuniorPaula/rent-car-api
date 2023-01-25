da980b9241bf488df16cd61c39ea9bfcff259f52
Junior Paula <luke.junnior@icloud.com>
2023-01-25 15:11:37 -0300

    Implements uncaughtException and unhandledRejection when a error throws

--- src/main/server.js
+++ src/main/server.js
@@ -1,12 +1,21 @@
 import * as dotenv from 'dotenv'
 import { MongoDBProvider } from '../providers/mongodbProvider.js'
+import { logger } from '../providers/pinoProvider.js'
+import app from './app.js'
 
 dotenv.config()
 MongoDBProvider.connect(process.env.MONGODB_URL)
-  .then(async () => {
-    const app = (await import('./app.js')).default
-    app.listen(process.env.PORT, () =>
-      console.info(`server is running at port ${process.env.PORT}`),
-    )
-  })
-  .catch(console.error)
+  .then(() => logger.info('database connected'))
+  .catch(logger.error)
+
+app.listen(process.env.PORT, () =>
+  logger.info(`server is running at port ${process.env.PORT}`),
+)
+
+process.on('uncaughtException', (error, origin) => {
+  logger.error(`${origin} signal received. ${error}`)
+})
+
+process.on('unhandledRejection', (error) => {
+  logger.error(`unhandledRejection signal received. ${error}`)
+})
